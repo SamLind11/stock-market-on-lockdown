5ab3d27305960db9accd97a76a3e3f5df9f3cc6f
Lori <lorigirton@outlook.com>
2023-10-05 16:02:51 -0400

    pre pull

--- static/js/code.js
+++ static/js/code.js
@@ -80,3 +80,66 @@ function generateLineGraph(selection) {
 
 // Generate initial graph on page load.
 generateLineGraph('finance');
+
+//Create a grouped bar chart for average stock price by sector for three time periods.
+function generateGroupBarChart(selection) {
+    let userChoice = meta[selection];
+
+    // Create initial graph.
+    let stockData = [];
+
+    layout = {
+        title: selection === 'finance' ? 'Finance Stocks':
+               selection === 'socialMedia' ? 'Social Media Stocks':
+               selection === 'healthcare' ? 'Healthcare Stocks': 'Retail Stocks'
+        };
+    
+        //Calculate the avearge values for each sector.
+        let sectorAverages = [];
+
+        stockData.forEach(function(stock) {
+            if (!sectorAverages[stock.sector]) {
+                sectorAverages[stock.sector] = {
+                    total: 0,
+                    count: 0
+                };
+            }
+            sectorAverages[stock.sector].total += stock.value;
+            sectorAverages[stock.sector].count++;
+        });
+
+        for(var sector in sectorAverages) {
+            sectorAverages[sector].averag = sectorAverages[sector].total / sectorAverages[sector].count;
+        }
+
+        //Create Greouped Bar Chart
+        var sectors = Object.keys(sectorAverages);
+        var averageValues = sectors.map(function(sector) {
+            return sectorAverages[sector].average;
+        });
+
+        var ctx = document.getElementById("grouped-bar-chart").getContext("2d");
+
+        var chart = new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: sectors,
+                datasets: [
+                    {
+                        label: "Average Stock Value",
+                        data: averageValues,
+                        backgroundColor: "rgba(75, 192, 192, 0.2)", 
+                        borderColor: "rgba(75, 192, 192, 1)",
+                        borderWidth: 1
+                    }]
+                }
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+        });
