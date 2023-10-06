f8932942f51167a58d9db01715348e7051ef5695
Lori <lorigirton@outlook.com>
2023-10-06 12:44:00 -0400

    pre pull

--- static/js/css/js/code.js
+++ static/js/css/js/code.js
@@ -77,70 +77,58 @@ function generateLineGraph(selection) {
             Plotly.addTraces("multiplot", trace);
         });
     }
+    generateBarGraph(selection);
 }
 
 // Generate initial graph on page load.
 generateLineGraph('finance');
 
-//Create a grouped bar chart for average stock price by sector for three time periods.
-function generateGroupBarChart(selection) {
+//Create Percent Change Bar Chart
+function generateBarGraph(selection) {
     let userChoice = meta[selection];
 
     // Create initial graph.
-    let stockData = [];
-
-    layout = {
+    let layout = {
+        barmode: 'stack',
         title: selection === 'finance' ? 'Finance Stocks':
                selection === 'socialMedia' ? 'Social Media Stocks':
-               selection === 'healthcare' ? 'Healthcare Stocks': 'Retail Stocks'
-        };
-    
-        //Calculate the avearge values for each sector.
-        let sectorAverages = [];
+               selection === 'healthcare' ? 'Healthcare Stocks': 'Retail Stocks', 
+        width: 1500,
+        height: 700
+    };
+    let data = [];
 
-        stockData.forEach(function(stock) {
-            if (!sectorAverages[stock.sector]) {
-                sectorAverages[stock.sector] = {
-                    total: 0,
-                    count: 0
+    for (let i = 0; i < userChoice.length; i++){
+        // Generate url.
+        let stockSymbol = userChoice[i].symbol; 
+        let stockName = userChoice[i].name;
+        let link = `http://127.0.0.1:5000/api/data/${stockSymbol}`;
+        console.log(link);
+        // Fetch json data from Flask API.
+        d3.json(link).then(response => {
+                
+                dates= [];
+                highs = [];
+                for (let i = 0; i < response.length; i++) {
+                    dates.push(response[i]['date'])
+                    highs.push(response[i]['high'])
+                }
+                // New code (calculate percent change)
+                
+
+                // Create the trace for the data.
+                trace = {
+                    x: dates,
+                    y: highs,//************Change to percent change
+                    type: "bar",
+                    name: `${stockName} (${stockSymbol.toUpperCase()})`,
+                    text: stockSymbol.toUpperCase()
                 };
-            }
-            sectorAverages[stock.sector].total += stock.value;
-            sectorAverages[stock.sector].count++;
+    
+                // Add the traces to the plot.
+                // Plotly.addTraces("bar", trace);
+                Plotly.newPlot("bar", [trace], layout);
+
         });
-
-        for(var sector in sectorAverages) {
-            sectorAverages[sector].averag = sectorAverages[sector].total / sectorAverages[sector].count;
-        }
-
-        //Create Greouped Bar Chart
-        var sectors = Object.keys(sectorAverages);
-        var averageValues = sectors.map(function(sector) {
-            return sectorAverages[sector].average;
-        });
-
-        var ctx = document.getElementById("grouped-bar-chart").getContext("2d");
-
-        var chart = new Chart(ctx, {
-            type: "bar",
-            data: {
-                labels: sectors,
-                datasets: [
-                    {
-                        label: "Average Stock Value",
-                        data: averageValues,
-                        backgroundColor: "rgba(75, 192, 192, 0.2)", 
-                        borderColor: "rgba(75, 192, 192, 1)",
-                        borderWidth: 1
-                    }]
-                }
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }]
-                    }
-                }
-        });
+    }
+}
